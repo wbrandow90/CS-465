3835697800ce8e92000222e8ce9495007f00614b
William Brandow <william.a.brandow@gmail.com>
2023-06-13 15:35:41 -0400

    module7

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload',
+    algorithms: ["HS256"],
+});
 
+const authController = require('../controllers/authentication');
 const tripsController = require('../controllers/trips');
 const roomsController = require('../controllers/rooms');
 
 router
+    .route('/login')
+    .post(authController.login);
+
+router
+    .route('/register')
+    .post(authController.register);    
+
+router
     .route('/trips')
     .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
+    .post(auth, tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindCode) 
-    .put(tripsController.tripsUpdateTrip);   
+    .put(auth, tripsController.tripsUpdateTrip)
+    .delete(auth, tripsController.tripsDeleteTrip);  
 
 router
     .route('/rooms') 
